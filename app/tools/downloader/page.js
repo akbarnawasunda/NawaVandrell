f9b934dff323ab14ada45d5d6fbe1ba2b6aaf9b8
akbarnawasunda <akbarnawasunda@gmail.com>
2026-08-10 16:20:02 +0700

    fix: isolate client code with ssr:false to avoid prerender errors

--- app/tools/downloader/page.js
+++ app/tools/downloader/page.js
@@ -1,5 +1,13 @@
-'use client';
-// ... (kode lengkap dari jawaban sebelumnya)
+import dynamic from 'next/dynamic';
 
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+export const runtime = 'nodejs';
+
+const DownloaderClient = dynamic(() => import('./DownloaderClient'), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <DownloaderClient />;
+}
